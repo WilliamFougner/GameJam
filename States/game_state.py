65d3c18b5537148a4d079049373c9f4ed4b63703
Willy <cwilliamfougner@gmail.com>
2026-03-24 13:04:52 +0100

    la til spillobjekt, fiender og spillere

--- States/game_state.py
+++ States/game_state.py
@@ -2,8 +2,10 @@
 Dette er staten for spillet. Det er her du legger til Spillobjekter, logikk, etc...
 """
 
+
 from states.base_state import BaseState
 import pygame
+
 
 class GameState(BaseState):
     def __init__(self):
@@ -26,3 +28,37 @@ class GameState(BaseState):
     def draw(self, surface: pygame.Surface):
         surface.fill((0, 0, 0))
         self.draw_text(surface, "Du er i spillet! Trykk ESC for å gå tilbake til hovedmenyen.", self.font, (255, 255, 255), (250, 250))
+
+class Spillobjekt():
+    def __init__(self, x, y, width, height):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.rect = pygame.Rect(x - width // 2, y - height // 2, width, height)
+
+class Spiller(Spillobjekt):
+    def __init__(self, x, y):
+        super().__init__(x, y, width = 10, height = 10)
+        self.color = (0, 200, 0)
+        self.speed = 2
+        self.hp = 3
+
+    def update(self, dt):
+        self.rect = pygame.Rect(self.x - self.width // 2, self.y - self.height // 2, self.width, self.height)
+
+    def draw(self, surface):
+        pygame.draw.rect(surface, self.color, self.rect)
+
+class Fiende(Spillobjekt):
+    def __init__(self, x, y):
+        super().__init__(x, y, width = 20, height = 20)
+        self.color = (200, 0, 0)
+        self.speed = 2
+        self.hp = 3
+
+    def update(self, dt):
+        self.rect = pygame.Rect(self.x - self.width // 2, self.y - self.height // 2, self.width, self.height)
+
+    def draw(self, surface):
+        pygame.draw.rect(surface, self.color, self.rect) 
