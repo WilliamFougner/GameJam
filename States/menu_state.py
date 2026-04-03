96bb9bc49690a77443ea9b7133cbcc15ca0e3817
Vandc10 <pvandawtceu@gmail.com>
2026-04-03 15:23:55 +0200

    jobber med menu knapper, ER IKKE FERDIG

--- States/menu_state.py
+++ States/menu_state.py
@@ -12,14 +12,19 @@ class MenuState(BaseState):
         self.image = pygame.transform.scale(self.image, (1000, 600))
         self.font = pygame.font.SysFont("Arial", 24)
 
+        self.knfortsett = knapp(310, 275, 380, 105)
+        self.knstopp = knapp(310, 440, 380, 105)
+        
+
     def handle_events(self, events : list[pygame.event.Event]):
         for event in events:
             if event.type == pygame.QUIT:
                 self.next_state = None
                 self.done = True
             
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_SPACE:
+
+            if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1: #Venstre mus knapp er 1 
+                if self.knfortsett.sjekk_klikk(event.pos):
                     self.next_state = "GAME"
                     self.done = True
 
@@ -28,6 +33,22 @@ class MenuState(BaseState):
 
     def draw(self, surface: pygame.Surface):
         surface.blit(self.image, (0, 0))
+
+        self.knfortsett.draw(surface)
+        self.knstopp.draw(surface)
          
         
         self.draw_text(surface, "Welcome! to the game Trykk SPACE for å starte.", self.font, (255, 255, 255), (500, 400))
+
+class knapp():
+    def __init__(self, x, y, width, height, tykkelse = 2):
+        self.rect = pygame.Rect(x, y, width, height, )
+        self.farge = (255, 0, 0)
+        self.tykkelse = tykkelse
+
+    
+    def draw(self, surface):
+         pygame.draw.rect(surface, self.farge, self.rect, self.tykkelse)
+    
+    def sjekk_klikk(self,mouse_pos):
+        return self.rect.collidepoint(mouse_pos)
